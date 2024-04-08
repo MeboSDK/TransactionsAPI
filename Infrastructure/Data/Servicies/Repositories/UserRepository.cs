5eed60256da9078a8d6fb6262964bf94990a7611
Merab Mosiashvili <48407417+merabmos@users.noreply.github.com>
2024-04-08 16:43:46 +0400

    Remove unnecessary "await" and "async" keywords from UserRepository

--- Infrastructure/Data/Servicies/Repositories/UserRepository.cs
+++ Infrastructure/Data/Servicies/Repositories/UserRepository.cs
@@ -7,18 +7,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Infrastructure.Data.Servicies.Repositories
-{
-    public class UserRepository(TransactionDBContext transactionDbContext) : Repository<User>(transactionDbContext), IUserRepository
-    {
-        public async Task<User> FindByEmailAsync(string email)
-        {
-            return await _dbContext.Users.FirstOrDefaultAsync(o => o.Email == email);
-        }
+namespace Infrastructure.Data.Servicies.Repositories;
 
-        public async Task<bool> IsEmailUniqueAsync(string email)
-        {
-            return await _dbContext.Users.AllAsync(u => u.Email != email);
-        }
+public class UserRepository(TransactionDBContext transactionDbContext) : Repository<User>(transactionDbContext), IUserRepository
+{
+    public Task<User> FindByEmailAsync(string email)
+    {
+        return _dbContext.Users.FirstOrDefaultAsync(o => o.Email == email);
+    }
+
+    public Task<bool> IsEmailUniqueAsync(string email)
+    {
+        return _dbContext.Users.AllAsync(u => u.Email != email);
     }
 }
